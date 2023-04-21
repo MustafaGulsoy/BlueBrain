fe106bccaee196dc79c9e740963570c82930da54
Mustafa GÜLSOY <mustafa30045@gmail.com>
2023-04-21 16:26:50 +0300

    Update AITONeuralNetwork.py
    Fit bar changed,
    Multiple Training network can train

--- AITONeuralNetwork.py
+++ AITONeuralNetwork.py
@@ -1,12 +1,28 @@
+import os
+import pickle
 import random
-import numpy as np
+import sys
 from math import e
 import time
-import pickle
-import csv
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 
+
+def updt(total, progress):
+    """
+    Displays or updates a console progress bar.
+
+    Original source: https://stackoverflow.com/a/15860757/1391441
+    """
+    barLength, status = 100, ""
+    progress = float(progress) / float(total)
+    if progress >= 1.:
+        progress, status = 1, "\r\n"
+    block = int(round(barLength * progress))
+    text = "\r[{}] {:.0f}% {}".format(
+        "#" * block + "-" * (barLength - block), round(progress * 100, 0),
+        status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
 
 def sigmoid_function(x):
     return 1 / (1 + e * (1 / x))
@@ -23,26 +39,38 @@ class AITONeuralNetwork:
         self.generation_count = generation_count
         self.generation_weight = secret_layer_size * secret_layer_size * (
                 secret_layer_count - 1) + input_layer_size * secret_layer_size
+        self.model_path = os.getcwd()
 
     def set_up(self):
         self.create_network_weight()
         self.split_data()
 
-    def load_model(self):
-        with open('AITO.dat', 'rb') as f:
+    def load_model(self, path=None):
+
+        if path is None:
+            with open('AITO.dat', 'rb') as f:
+                self = pickle.load(f)
+                return self
+        with open(path, 'rb') as f:
             self = pickle.load(f)
             return self
 
-    def save_model(self):
-        with open('AITO.dat', 'wb') as f:
-            pickle.dump(self, f)
+    def save_model(self, path=None):
+
+        if path is None:
+            with open('../Models/AITO.dat', 'wb') as f:
+                pickle.dump(self, f)
+
+        else:
+            with open(path, 'wb') as f:
+                pickle.dump(self, f)
 
     def create_network_weight(self):
         for generation in range(self.generation_count):
             self.network_weight.append([])
             for i in range(self.generation_weight):
                 self.network_weight[generation].append(round(random.uniform(-1.0, 1.0),
-                                                             5))
+                                                             5) / 50)
 
     def split_data(self):
         for generation in range(self.generation_count):
@@ -61,7 +89,7 @@ class AITONeuralNetwork:
             for j in range(len(self.network_weight[i])):
                 new_data[i].append([])
                 for t in range(len(self.network_weight[i][j])):
-                    new_data[i][j].append(self.network_weight[i][j][t] + round(random.uniform(-10.0, 10.0),
+                    new_data[i][j].append(self.network_weight[i][j][t] + round(random.uniform(-1.0, 1.0),
                                                                                5) / 100)
 
         return new_data
@@ -73,7 +101,6 @@ class AITONeuralNetwork:
             for x in input_x:
                 if len(current_y[i]) < len(input_x):
                     current_y[i].append([])
-
                 current_y[i][t] = (np.dot(x, self.network_weight[i][0:self.input_layer_size]))
 
                 index = 0
@@ -85,7 +112,7 @@ class AITONeuralNetwork:
                                 self.network_weight[i][
                                 self.input_layer_size + self.secret_layer_size * index: self.input_layer_size + self.secret_layer_size * index + self.secret_layer_size]))
                         index = index + 1
-                        t = t + 1
+                t = t + 1
         return current_y
 
     # *--------------------------------------------------------------------------------------------------------------*
@@ -108,7 +135,7 @@ class AITONeuralNetwork:
                                 new_weights[i][
                                 self.input_layer_size + self.secret_layer_size * index: self.input_layer_size + self.secret_layer_size * index + self.secret_layer_size]))
                         index = index + 1
-                        t = t + 1
+                t = t + 1
         return new_y
 
     def generate_new_population(self, output_y, current_result):
@@ -146,6 +173,11 @@ class AITONeuralNetwork:
                 pass
 
     def fit(self, input_x, output_y, iteration, genetic_iteration):
+
+        if self.inputIsNotValid(input_x):
+            print('\033[1;31m' + "Invalid input, shape of data doesn't fit model.")
+            print('\033[1;31m' + "Check fit function.")
+            sys.exit()
         current_y = []  # predict output by current network weight
         new_y = []  # predict output by make-up network weight
 
@@ -155,10 +187,12 @@ class AITONeuralNetwork:
 
         for generation in range(genetic_iteration):
             for rep in range(iteration):
-                print("================================== Iteration : " + str(
-                    int(rep + 1)) + " =======================================")
-                print(
-                    "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+                updt(iteration * genetic_iteration, (generation * rep + rep))
+
+                # print("================================== Generation: " + str(generation + 1) + " Iteration : " + str(
+                #     int(rep + 1)) +  " =======================================")
+                # print(
+                #     "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
                 new_weights = self.create_new_weights()
 
                 current_result = self.predict_with_current_weight(input_x, current_y)
@@ -171,6 +205,10 @@ class AITONeuralNetwork:
             self.generate_new_population(output_y, current_result)
 
     def predict(self, input_x):
+        if self.inputIsNotValid(input_x):
+            print('\033[1;31m' + "Invalid input, shape of data doesn't fit model.")
+            print('\033[1;31m' + "Check predict function.")
+            return []
         y = []
 
         t = 0
@@ -191,42 +229,17 @@ class AITONeuralNetwork:
 
         return y
 
+    def inputIsNotValid(self, input_x):
 
-input_layer_size = 3
-secret_layer_size = 3
-secret_layer_count = 2
-generation_count = 1
+        if type(input_x[0]) is int:
+            if self.input_layer_size != len(input_x):
+                return True
+            return False
 
+        else:
 
-nn2 = AITONeuralNetwork(input_layer_size, secret_layer_size, secret_layer_count,
-                        generation_count)
-
-nn2 = nn2.load_model()
-
-y2 = nn2.predict([418.504078221616,113.077935462083,45.0876857639276])
-
-
-
-
-
-# nn.set_up()
-#
-#
-#
-# nn.fit(input_x=[[0.1, 0.3, 0.5], [0.6, 0.1, 0.8]],
-#        output_y=[[0.9, 0.2, 0.2], [0.3, 0.1, 0.6]], iteration=2, genetic_iteration=1)
-
-# y = nn.predict([0.6, 0.1, 0.8])
-# nn.save_model()
-
-# nn2 = nn2.load_model()
-#
-# y2 = nn2.predict([418.504078221616,113.077935462083,45.0876857639276])
-
-
-
-
-
-
+            if self.input_layer_size != len(input_x[0]):
+                return True
+            return False
 
 
